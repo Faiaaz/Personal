ed379bb7d1d2929f5b7b94f9f4a9294ad0b600dc
Fahim Faiaz <fahim7faiaz@gmail.com>
2020-07-25 13:05:00 +0600

    --

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class PersonInfo(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100, unique=True)
+    # image = models.ImageField(max_length=100)
--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .forms import NewUserForm
 
 
 # Create your views here.
 def index(request):
     return render(request, 'crud/index.html')
+
+
+def user_info(request):
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('form invalid!')
+    return render(request, 'crud/users.html', {'form': form})
